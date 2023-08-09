f3e04eb04e24c25fe492db5cf7d9bcb63db0880c
Deep5656 <deepanshukatiyar5656@gmail.com>
2023-08-09 14:59:55 +0530

    initial commit in aman branch

--- loandispersalsystem/src/main/java/com/lds/loandispersalsystem/LoandispersalsystemApplication.java
+++ loandispersalsystem/src/main/java/com/lds/loandispersalsystem/LoandispersalsystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class LoandispersalsystemApplication {
-
+//new branch name aman
 	public static void main(String[] args) {
 		SpringApplication.run(LoandispersalsystemApplication.class, args);
 	}
